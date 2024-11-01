55c8d34355e7eb72ed17bf4baeeb46b7c530d7b2
malak <mrvda225@gmail.com>
2024-11-01 19:55:04 +0200

    user model

--- controlers/userControlers.js
+++ controlers/userControlers.js
@@ -1,3 +1,4 @@
+const { status } = require("express/lib/response");
 
 
 
@@ -10,11 +11,29 @@ const register=async(req,res)=>{
 }
 
 
-const log_in=async(req,res)=>{
+const log_in=async(async(req,res,next)=>{
 
-
-
+const {email, password}=req.body;
+if(!email&&!password){
+    const error= appError.create('email a password ara required',400, httpStatusText.FAIL)
+    return next(error);
 }
+const user=await user.findOne({email:email});
+
+    const matchedPassword= await bcrypt.compare(password,user.password);
+    if(!user){
+        const error= appError.create('email a password ara required',401, httpStatusText.FAIL)
+        return next(error);
+    }
+    if (user&&matchedPassword ){
+        return res.json({status:httpStatusText.SUCCESS, data:{user:'log in successfully'}});
+    }
+
+else{
+    const error= appError.create('something wrong',402, httpStatusText.ERROR)
+    return next(error);
+}
+})
 
 
 module.exports={
